47a01e898b90edb47f796da87493c9bb2bfa3855
savioferraz <savio.ferraz88@gmail.com>
2023-04-06 04:41:08 -0300

    feat: add tweets pagination

--- src/main/java/com/tweteroo/api/controllers/TweetsController.java
+++ src/main/java/com/tweteroo/api/controllers/TweetsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,9 +44,9 @@ public class TweetsController {
         return services.getUserTweets(username);
     }
 
-    @GetMapping("/page/{page}")
+    @GetMapping(params = "page")
     @ResponseStatus(value = HttpStatus.OK)
-    public List<Tweet> getTweetByPage(@PathVariable int page) {
+    public List<Tweet> getTweetByPage(@RequestParam int page) {
         return services.getTweetsByPage(page);
     }
 }
